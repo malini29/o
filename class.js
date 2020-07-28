5f3e23b579d82afdef2ba68cdaa1346956881c59
malini29 <68008403+malini29@users.noreply.github.com>
2020-07-28 05:04:51 -0700

    Add files via upload

---
+++ class.js
@@ -0,0 +1,29 @@
+class Log
+
+var options = {
+restitution:1;
+}
+
+this.body bodies.rectangle(x,y,width,,height,options);
+World.add= (world,this.body);
+
+
+display (){
+  var  pos -this.body.position;
+  rectMode=(CENTER);
+  fill(255);
+  rect(pos.x,pos.y,this.height,this.width);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
